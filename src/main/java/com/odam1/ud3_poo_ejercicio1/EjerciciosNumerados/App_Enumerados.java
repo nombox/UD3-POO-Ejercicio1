49c500db21a9086e23d7763782b20612d5fcf10a
miPcLocal <sebastianvargas2026@gmail.com>
2025-12-14 20:02:26 +0100

    Agregados comentarios en el ejercicio 4

--- src/main/java/com/odam1/ud3_poo_ejercicio1/EjerciciosNumerados/App_Enumerados.java
+++ src/main/java/com/odam1/ud3_poo_ejercicio1/EjerciciosNumerados/App_Enumerados.java
@@ -94,6 +94,21 @@ public class App_Enumerados {
     }
     
     static void ejercicio4(){
+        /*Define una enumeración llamada NivelEstudios con valores: PRIMARIA,
+        SECUNDARIA, CICLOS, UNIVERSIDAD.
+        
+          Define también una clase llamada Estudiante que tenga como atributos: nombre, edad y nivel
+        de estudios. Haz los atributos privados e incluye getters y setters.
+        
+          Haz un programa que solicite al usuario los datos de un alumno, y si todos son correctos, cree
+        una instancia de Alumno y muestre el método toString del mismo, que permitirá visualizar
+        todos sus atributos en una línea. 
+        
+          Los datos son correctos:
+            *si el nombre ocupa más de 3 caracteres. 
+            *la edad está entre 0 y 120 y el nivel de estudios introducido se corresponde con un
+                valor de la enumeración definida*/
+        
      //Creo el objeto
         Estudiante nuevoEstudiante = new Estudiante();
         //booleano para saber si todo esta correcto
